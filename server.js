ae3d6f7b2df60fe955e0deb38552b75e3e2b3fb3
hashcookie <hashcookie404@gmail.com>
2024-05-05 23:10:27 +0800

    refactor(auth): extract addAuthParams function from server.js

--- server.js
+++ server.js
@@ -1,54 +1,15 @@
 const express = require("express");
 const https = require("https");
 const querystring = require("querystring");
-const crypto = require("crypto");
-const uuid = require("uuid");
+const { addAuthParams } = require("./auth");
 const app = express();
 app.use(express.json());
 
 const cors = require("cors");
 app.use(cors());
 
-// API密钥和密钥xxxxx
 const APP_KEY = "62c2cdf10e77d810";
 const APP_SECRET = "OIQC0HxQzq0dT7ItonlA7z1DMTYFdGxp";
-
-// 鉴权参数添加函数
-function addAuthParams(appKey, appSecret, params) {
-  const q = params.q || params.img;
-  const salt = uuid.v1();
-  const curtime = Math.floor(Date.now() / 1000).toString();
-  const sign = calculateSign(appKey, appSecret, q, salt, curtime);
-
-  params.appKey = appKey;
-  params.salt = salt;
-  params.curtime = curtime;
-  params.signType = "v3";
-  params.sign = sign;
-}
-
-// 签名计算函数
-function calculateSign(appKey, appSecret, q, salt, curtime) {
-  const input = getInput(q);
-  const strSrc = appKey + input + salt + curtime + appSecret;
-  return encrypt(strSrc);
-}
-
-// 加密函数
-function encrypt(strSrc) {
-  const hash = crypto.createHash("sha256");
-  hash.update(strSrc);
-  return hash.digest("hex");
-}
-
-// 输入处理函数
-function getInput(input) {
-  if (!input) return input;
-  const inputLen = input.length;
-  return inputLen <= 20
-    ? input
-    : input.substring(0, 10) + inputLen + input.substring(inputLen - 10);
-}
 
 // 发送请求函数
 function doCall(url, headers, params, method) {
