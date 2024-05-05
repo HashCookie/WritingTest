ff10a878c6a13e5cc1c3c60ab0fbde3352b96023
hashcookie <hashcookie404@gmail.com>
2024-05-05 23:26:05 +0800

    chore: Add .gitignore, update config.js, package.json

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 module.exports = {
-  APP_KEY: "62c2cdf10e77d810",
-  APP_SECRET: "OIQC0HxQzq0dT7ItonlA7z1DMTYFdGxp",
+  APP_KEY: process.env.APP_KEY,
+  APP_SECRET: process.env.APP_SECRET,
   API_URL: "https://openapi.youdao.com/v2/correct_writing_text",
   PORT: process.env.PORT || 3000,
 };
